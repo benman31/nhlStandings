44b9e071b5f1e5a68c280f0a1a00e6bd1e4ae486
benman87 <benman@upei.ca>
2020-11-21 15:58:38 -0800

    Updated HockeyDriver with necessary code to process input arguments and detect invalid input

--- src/main/java/HockeyDriver.java
+++ src/main/java/HockeyDriver.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.IOException;
+
 
 /**
  * Handle the assignment command line arguments
@@ -17,5 +19,44 @@ public class HockeyDriver {
             System.out.println("Example: -f nhl-game-data" + File.separator + "games-nov.csv (to load new game results and display resulting standings");
             System.exit(0);
         }
+        else{
+            if (args[0].equals("-t")){
+                if (HockeyData.isTeamID(args[1])) {
+
+                    int teamID = Integer.parseInt(args[1]);
+
+                    try {
+                        StandingsPrinter.print(teamID);
+                    } catch (IOException exception) {
+                        System.out.println(exception.getMessage());
+                    }
+                }
+                else {
+                    System.out.println("Invalid number, please use an integer from 1 to 30 inclusive as second argument");
+                }
+
+            }
+            else if (args[0].equals("-f")){
+                for(int i= 1; i < args.length; i++){
+                    try{
+                        HockeyData.parseResult(args[i]);
+                    }
+                    catch(IOException exception){
+                        System.out.println(exception.getMessage());
+                    }
+                 }
+                try {
+                    StandingsPrinter.printAll();
+                }
+                catch (IOException exception){
+                    System.out.println(exception.getMessage());
+                }
+            }
+            else {
+                System.out.println("Invalid command");
+            }
+        }
+
     }
+
 }
